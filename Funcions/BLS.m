1be02ad92158fa74dd1e9916b222fe096249c190
Didac <didac.emdn@gmail.com>
2022-02-23 23:08:15 +0100

    Mensajito de no trobar solucio

--- Funcions/BLS.m
+++ Funcions/BLS.m
@@ -6,4 +6,7 @@ function [ak, iWout] = BLS(xk, f, df, dk, amax, amin, p, c1, c2, iW)
         ak = p*ak;
         [b, iWout] = WolfeC(xk, ak, f, df, dk, c1, c2, iW);
     end
+    if iWout < 2
+        disp('No solution found');
+    end
 end
