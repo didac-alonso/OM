f5d79691eb8c8d155fdaaa20e06c2cb03c1997a8
Didac <didac.emdn@gmail.com>
2022-02-25 15:01:12 +0100

    Comentaris

--- Funcions/BLS.m
+++ Funcions/BLS.m
@@ -1,3 +1,15 @@
+%{
+Input:
+    - xk: punt actual
+    - dk: direcció de descens
+    - c1, c2: per Wolfe
+    - p: rho, com fem descendir alpha
+    - a: alpha, donada per interval amax, amin
+    - iW: metode Wolfe a usar
+Output:
+    - ak: alpha òptima
+    -iWout: indicador de les condicions que compleix ak
+%}
 function [ak, iWout] = BLS(xk, f, df, dk, amax, amin, p, c1, c2, iW)
     ak = amax;
     iWout = 0;
--- Funcions/WolfeC.m
+++ Funcions/WolfeC.m
@@ -1,3 +1,14 @@
+%{
+Input:
+    - xk: punt actual
+    - ak: alpha actual
+    - dk: direcció de descens
+    - c1, c2: constants per WC
+    - iW: metode Wolfe a usar
+Output:
+    - b: cert si cumpleix totes les WC especificades per iW
+    - iWout: indicador de les condicions que compleix ak
+%}
 function [b, iWout] = WolfeC(xk, ak, f, df, dk, c1, c2, iW)
     i = f(xk+ak.*dk) <= f(xk) + c1.*df(xk)'*dk*ak;
     iWout = 0;
