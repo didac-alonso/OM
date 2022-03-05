c2af0e1e80e9d3fe58360a6d6479787da693ef3c
Didac <didac.emdn@gmail.com>
2022-03-05 12:18:45 +0100

    solver i cgm

--- Funcions/CGM.m
+++ Funcions/CGM.m
@@ -11,48 +11,48 @@ Input:
     - iW: Condicions Wolfe a satisfer:
     - tol: precisió buscada
     - itmax: màxim d'iteracions que pot dur a terme l'algoritme
+    - icd:  variant de CGM icd = 1 FR, icd = 2 PR+
+    - irc: Restart per la CGM, irc = no, irc = 1 si
+    - nu: nombre de iteracions entre restarts en cas de que irc = 1
 
 Output:
     - x: Punt que fa 0 df.
     - it: nombre d'iteracions usades
 %}
-function [xk, dk, ak, Bk, iWk, it] = CGM(x, f, df, amin, amax, p, c1, c2, iW, tol, itmax, isd, icd, irc, nu)
+function [xk, dk, ak, Bk, iWk, it] = CGM(x, f, df, amin, amax, p, c1, c2, iW, tol, itmax, icd, irc, nu)
     it = 0;
-    pp = p;
     xk = [x];
     Bk = [];
-    dfxk = df(x);
-    dk = [-dfxk];
-    d = -dfxk;
+    dfx = df(x);
+    dk = [-dfx];
+    d = -dfx;
     ak = [];
     iWk = [];
-    while norm(dfxk) > tol & it < itmax
+    restart = 0;
+    if irc == 0
+        nu = 3;
+    end
+    while norm(dfx) > tol & it < itmax
         [a, iWout] = BLS(x,f,df,d,amin,amax, p, c1, c2, iW);
         iWk = [iWk iWout];
         ak = [ak a];
         x = x + a*d;
-        xk = [xk x]; 
-
-        if isd == 2
-            if icd == 1
-               B  
-            end
-            B = 
-        end
-        dfxk = -df(x);
-        if isc == 1 | restart == nu
-            d = -dfxk;
+        xk = [xk x];
+        dfx1 = dfx;
+        dfx = df(x);
+        % Si irc == 0 no augmentarà
+        restart = restart + irc;
+        if restart == nu
+            restart = 0;
+            B = 0;
+        elseif icd == 1
+            B = dfx'*dfx/(norm(dfx1))^2;
         else
-            B = CGM(dfk, )
+            B = max(0, (dfx'*(dfx-dfx1))/(norm(dfx1))^2);   
         end
-        %{
-        
-        % Si no troba alpha, cal fer més exhaustiva la cerca
-        if iWout < 2
-            p = 1.1*p;
-        %}
+        Bk = [Bk B];
+        d = -dfx + B*dk(end);
+        dk = [dk d];
         it = it + 1;
-        dfxk = df(x);
     end
-
 end
--- Funcions/solver.m
+++ Funcions/solver.m
@@ -6,37 +6,7 @@ function [xk, dk, ak, iWk, betak, it] = solver(x, f, df, d2f, amin, amax, p, c1,
     betak = [];
     if isd == 1
        [xk, dk, ak, iWk, it] = GM(x, f, df, amin, amax, p, c1, c2, iW, tol, itmax);
-    else
-        
+    elseif isd == 2
+        [xk, dk, ak, betak, iWk, it] = CGM(x, f, df, amin, amax, p, c1, c2, iW, tol, itmax, icd, irc, nu);
     end
-    
-    while norm(dfxk) > tol & it < itmax
-        [a, iWout] = BLS(x,f,df,d,amin,amax, p, c1, c2, iW);
-        iWk = [iWk iWout];
-        ak = [ak a];
-        x = x + a*d;
-        xk = [xk x]; 
-
-        if isd == 2
-            if icd == 1
-               B  
-            end
-            B = 
-        end
-        dfxk = -df(x);
-        if isc == 1 | restart == nu
-            d = -dfxk;
-        else
-            B = CGM(dfk, )
-        end
-        %{
-        
-        % Si no troba alpha, cal fer més exhaustiva la cerca
-        if iWout < 2
-            p = 1.1*p;
-        %}
-        it = it + 1;
-        dfxk = df(x);
-    end
-
 end
