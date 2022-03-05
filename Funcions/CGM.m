27784fba0710479a2dad068c9e2bc437b513a010
Didac <didac.emdn@gmail.com>
2022-03-05 13:06:14 +0100

    cgm

--- Funcions/CGM.m
+++ Funcions/CGM.m
@@ -11,15 +11,15 @@ Input:
     - iW: Condicions Wolfe a satisfer:
     - tol: precisió buscada
     - itmax: màxim d'iteracions que pot dur a terme l'algoritme
-    - icd:  variant de CGM icd = 1 FR, icd = 2 PR+
-    - irc: Restart per la CGM, irc = no, irc = 1 si
+    - icg:  variant de CGM icg = 1 FR, icg = 2 PR+
+    - irc: Restart per la CGM, irc = no, irc = 1 RC1, irc = 2 RC2
     - nu: nombre de iteracions entre restarts en cas de que irc = 1
 
 Output:
     - x: Punt que fa 0 df.
     - it: nombre d'iteracions usades
 %}
-function [xk, dk, ak, Bk, iWk, it] = CGM(x, f, df, amin, amax, p, c1, c2, iW, tol, itmax, icd, irc, nu)
+function [xk, dk, ak, Bk, iWk, it] = CGM(x, f, df, amin, amax, p, c1, c2, iW, tol, itmax, icg, irc, nu)
     it = 0;
     xk = [x];
     Bk = [];
@@ -30,28 +30,32 @@ function [xk, dk, ak, Bk, iWk, it] = CGM(x, f, df, amin, amax, p, c1, c2, iW, to
     iWk = [];
     restart = 0;
     if irc == 0
-        nu = 3;
+        nu = Inf
     end
     while norm(dfx) > tol & it < itmax
         [a, iWout] = BLS(x,f,df,d,amin,amax, p, c1, c2, iW);
         iWk = [iWk iWout];
         ak = [ak a];
+        if iWout < 2
+            fprintf('%d\n',it)
+        end
         x = x + a*d;
         xk = [xk x];
-        dfx1 = dfx;
-        dfx = df(x);
-        % Si irc == 0 no augmentarà
-        restart = restart + irc;
-        if restart == nu
+        dfx1 = dfx; % df k-1
+        dfx = df(x); % df k
+        restart = restart + irc == 1;
+        if irc == 1 & restart == nu
             restart = 0;
             B = 0;
-        elseif icd == 1
-            B = dfx'*dfx/(norm(dfx1))^2;
+        elseif irc == 2 & abs(dfx'*dfx1)/(norm(dfx))^2 >= nu
+            B = 0;
+        elseif icg == 1
+            B = (dfx'*dfx)/(norm(dfx1))^2;
         else
             B = max(0, (dfx'*(dfx-dfx1))/(norm(dfx1))^2);   
         end
         Bk = [Bk B];
-        d = -dfx + B*dk(end);
+        d = -dfx + B*dk(:,end);
         dk = [dk d];
         it = it + 1;
     end
--- Funcions/solver.m
+++ Funcions/solver.m
@@ -1,12 +1,12 @@
 % isc = 1 GM, ISD = 2 CGM, ISD = 3 BFGS
 % icd:  variant de CGM icd = 1 FR, icd = 2 PR+
-% irc: Restart per la CGM, irc = no, irc = 1 si
+% irc: Restart per la CGM, irc = no, irc = 1 RC1, irc = 2 RC2
 % nu: nombre de iteracions entre restarts en cas de que irc = 1
-function [xk, dk, ak, iWk, betak, it] = solver(x, f, df, d2f, amin, amax, p, c1, c2, iW, tol, itmax, isd, icd, irc, nu)
+function [xk, dk, ak, iWk, betak, it] = solver(x, f, df, d2f, amin, amax, p, c1, c2, iW, tol, itmax, isd, icg, irc, nu)
     betak = [];
     if isd == 1
        [xk, dk, ak, iWk, it] = GM(x, f, df, amin, amax, p, c1, c2, iW, tol, itmax);
     elseif isd == 2
-        [xk, dk, ak, betak, iWk, it] = CGM(x, f, df, amin, amax, p, c1, c2, iW, tol, itmax, icd, irc, nu);
+        [xk, dk, ak, betak, iWk, it] = CGM(x, f, df, amin, amax, p, c1, c2, iW, tol, itmax, icg, irc, nu);
     end
 end
