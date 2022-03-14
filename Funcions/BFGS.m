cc17253340572917a05b2bcf484fc0ef99b0c5ab
Didac <didac.emdn@gmail.com>
2022-03-14 01:04:36 +0100

    BFGS

---
+++ Funcions/BFGS.m
@@ -0,0 +1,27 @@
+function [xk, dk, ak, Hk iWk, it] = BFGS(x, f, df, amin, amax, rho, c1, c2, iW, tol, itmax, Q)
+    I = eye(size(x,1));
+    H = I;
+    Hk = [H];
+    it = 1;
+    xk = [x];
+    dk = [];
+    ak = [];
+    iWk = [];
+    dfx = df(x);
+    while norm(dfx) > tol & it <= itmax
+        d = -H*dfx;
+        dk = [dk d];
+        [a, iWout] = BLS(x, f, df, d, amin, amax, rho, c1, c2, iW, Q);
+        ak = [ak a];
+        iWk = [iWk iWout];
+        x = x+a*d;
+        dfx1 = dfx;
+        dfx = df(x);
+        s = x-xk(:,end); y = dfx-dfx1; 
+        p = 1/(y'*s);
+        xk = [xk x];
+        H = (I-p*s*y')*H*(I-p*y*s')+p*(s*s');
+        Hk = cat(3, Hk, H); % concatenacio 3D
+        it = it + 1;
+    end
+end
--- Funcions/solver.m
+++ Funcions/solver.m
@@ -1,12 +1,16 @@
 % isd = 1 GM, ISD = 2 CGM, ISD = 3 BFGS
-% icd:  variant de CGM icd = 1 FR, icd = 2 PR+
+% icg:  variant de CGM icd = 1 FR, icd = 2 PR+
 % irc: Restart per la CGM, irc = no, irc = 1 RC1, irc = 2 RC2
 % nu: nombre de iteracions entre restarts en cas de que irc = 1
-function [xk, dk, ak, iWk, betak, it] = solver(x, f, df, d2f,tol, itmax, amax, amin, p, c1, c2, iW, isd, icg, irc, nu)
+function [xk, dk, alk, iWk, betak, Hk, tauk, it] = solver(x, f, g, h,epsG, kmax, almax, almin, rho, c1, c2, iW, isd, icg, irc, nu, delta)
     betak = [];
+    Hk = [];
+    tauk = [];
     if isd == 1
-       [xk, dk, ak, iWk, it] = GM(x, f, df, amin, amax, p, c1, c2, iW, tol, itmax, d2f(0));
+        [xk, dk, alk, iWk, it] = GM(x, f, g, almin, almax, rho, c1, c2, iW, epsG, kmax, h(zeros(size(x,1))));
     elseif isd == 2
-       [xk, dk, ak, betak, iWk, it] = CGM(x, f, df, amin, amax, p, c1, c2, iW, tol, itmax, icg, irc, nu, d2f(0));
+        [xk, dk, alk, betak, iWk, it] = CGM(x, f, g, almin, almax, rho, c1, c2, iW, epsG, kmax, icg, irc, nu, h(zeros(size(x,1))));
+    elseif isd == 3
+        [xk, dk, alk, Hk, iWk, it] = BFGS(x, f, g, almin, almax, rho, c1, c2, iW, epsG, kmax, h(zeros(size(x,1))));
     end
 end
